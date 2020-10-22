f9c5a20fb099945f375a0277137709c12769a041
Cássio Fernandes <cassio.rfernandes@gmail.com>
2020-10-22 02:05:54 -0300

    Encerra inscricao observable

--- src/app/pokemon/busca-pokemon/busca-pokemon.component.ts
+++ src/app/pokemon/busca-pokemon/busca-pokemon.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
 import { FormBuilder } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-busca-pokemon',
@@ -9,11 +10,13 @@ import { FormBuilder } from '@angular/forms';
 })
 export class BuscaPokemonComponent implements OnInit {
 
+  public dadosIncricao: Subscription;
+
   public listResultBusca: any[];
 
   profileForm = this.fb.group({
     busca: [null],
-    
+
   });
 
   constructor(
@@ -22,25 +25,29 @@ export class BuscaPokemonComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-  
+
   }
 
   onSubmit() {
     console.log(this.profileForm.value.busca)
-    this.buscaPokemon();  
+    this.buscaPokemon();
   }
 
   buscaPokemon() {
-    this.pokemonSevice.searchPokemon(this.profileForm.value.busca)
-    .subscribe(res=> {
-      this.listResultBusca = res['cards']
-      if(!res['cards'] || res['cards'].length === 0) {
-        return alert('Não foram encontrado resultados para sua busca')
-      }
-      return console.log(this.listResultBusca)
-    }, 
-      error => console.log(error)
-    );
+    this.dadosIncricao = this.pokemonSevice.searchPokemon(this.profileForm.value.busca)
+      .subscribe(res => {
+        this.listResultBusca = res['cards']
+        if (!res['cards'] || res['cards'].length === 0) {
+          return alert('Não foram encontrado resultados para sua busca')
+        }
+        return console.log(this.listResultBusca)
+      },
+        error => console.log(error)
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.dadosIncricao.unsubscribe();
   }
 
 }
--- src/app/pokemon/lista-pokemon/lista-pokemon.component.ts
+++ src/app/pokemon/lista-pokemon/lista-pokemon.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../pokemon.service';
-import { catchError } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-pokemon',
@@ -9,27 +9,36 @@ import { catchError } from 'rxjs/operators';
 })
 export class ListaPokemonComponent implements OnInit {
 
-  listPokemon: any[]
+  listPokemon: any[];
+
+  public listaIncricao: Subscription;
 
   constructor(
     private pokemonService: PokemonService
   ) { }
 
   ngOnInit(): void {
-    this.pokemonService.getListPokemon()
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    return this.listaIncricao = this.pokemonService.getListPokemon()
       .subscribe(res => {
         this.listPokemon = res['cards']
         this.ordenaNome(this.listPokemon);
-      
-      }, 
+
+      },
         error => console.log(error.message))
   }
-  
+
   ordenaNome(dadosArray) {
     return dadosArray.sort((a, b) => {
       return a.name < b.name ? -1 : a.name > b.name ? 1 : 0;
     });
   }
-  
+
+  ngOnDestroy(): void {
+    this.listaIncricao.unsubscribe();
+  }
 
 }
